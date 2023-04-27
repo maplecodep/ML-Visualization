fbc14dde2cf826d944bd2a26f6456c866bc0e535
maplecodep <anqzdata@gmail.com>
2023-04-26 18:53:29 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphen_e_dot = "-e ."
+# "-e ." in requirements.txt will map to setup.py
+def get_requirements(file_path:str)->List[str]:
+# this function will return the list of required packages
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n", " ") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+    return requirements
+
+setup(
+    name = 'ML-Visualization',
+    version='0.0.1',
+    author='maplecodep',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
